9a857253b7263544e3c245caf16124f2dc3cd95b
Parsa.zn <Parsaeagle@gmail.com>
2022-09-03 20:08:36 +0200

    Basic fetching done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const API_URL = "https://randomuser.me/api/";
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    
+    const fetchItems = async () => { //as we only use this to fetch items we can define the function here 
+      try{
+        const response = await fetch(API_URL)
+        const listItems = await response.json() ; 
+        setItems(listItems.results[0])
+        console.log(listItems.results[0])
+      } catch (e: unknown ){
+        console.log(e)
+      }
+    }
+
+    fetchItems();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
