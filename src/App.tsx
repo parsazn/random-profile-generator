39f2de25381ce7ceae1ca51bc331c94c8836fb79
Parsa.zn <Parsaeagle@gmail.com>
2022-09-04 20:37:01 +0200

    Basic profile section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
-import { Box, Grid, Skeleton, Stack } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Grid,
+  Button,
+  IconButton,
+  Paper,
+  Skeleton,
+  Stack,
+} from "@mui/material";
+import { borderRadius } from "@mui/system";
+import RefreshIcon from "@mui/icons-material/Refresh";
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 
@@ -63,23 +74,50 @@ function App() {
           return (
             <Box
               key={login.uuid}
-              sx={{ height: "85vh", m: 8, backgroundColor: "#587222" }}
+              sx={{ height: "85vh", m: 8, border: 1, borderColor: "#000" }}
             >
-              <Grid container spacing={2} sx={{ height: "100vh", p: 8 }}>
-                <Grid item xs={12} sm={4}>
-                  <Skeleton
-                    variant="rectangular"
-                    width={"100%"}
-                    height={"100%"}
-                  />
+              <Grid container spacing={2}>
+                <Grid item xs={12}  sm={3} sx={{ border: 1, m: 4 }}>
+                  <Box >
+                    <Grid
+                      container
+                      direction="column"
+                      justifyContent="center"
+                      alignItems="center"
+                      spacing={10}
+                    >
+                      <Grid item xs={12} >
+                        <Paper
+                          sx={{
+                            backgroundPositibon: "center",
+                            borderRadius: "50%",
+                            border: 1,
+                            backgroundSize: "cover",
+                            height: "300px",
+                            width: "300px",
+                            backgroundImage: `url(${picture.large})`,
+                          }}
+                        >
+                          <IconButton
+                            sx={{ backgroundColor: "#DDDDDD" }}
+                            title="Refresh"
+                          >
+                            <RefreshIcon aria-label="refresh" />
+                          </IconButton>
+                        </Paper>
+                      </Grid>
+                      <Grid item xs={12} sx={{border:1 , width:'100%'}} >
+                        <Stack spacing={3} >
+                          <Button variant="contained" fullWidth>Porn</Button>
+                          <Button variant="contained">Porn</Button>
+                          <Button variant="contained">Porn</Button>
+                          <Button variant="contained">Porn</Button>
+                        </Stack>
+                      </Grid>
+                    </Grid>
+                  </Box>
                 </Grid>
-                <Grid item xs={12} sm={8}>
-                  <Skeleton
-                    variant="rectangular"
-                    width={"100%"}
-                    height={"100%"}
-                  />
-                </Grid>
+                <Grid item xs={12} sm={9}></Grid>
               </Grid>
             </Box>
           );
